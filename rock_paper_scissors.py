1094c398d120139c2eeb41afcfc57119530664f2
Garima-Adhikari2 <dgarima23@tbc.edu.np>
2026-04-22 07:44:28 +0545

    error test

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -12,7 +12,7 @@ def get_computer_choice():
     return random.choice(['rock', 'paper', 'scissors'])
 
 def determine_winner(user_choice, computer_choice):
-    if user_choice == computer_choice:
+    
         return "It's a tie!"
     elif (user_choice == 'rock' and computer_choice == 'scissors') or \
          (user_choice == 'paper' and computer_choice == 'rock') or \
